625379cc890b1e9b3fc4455e65d0e42db3982512
Tyrasoul <bs3734ssst@gmail.com>
2026-06-11 21:05:27 +0530

    feat: included Weather conditions
    Also did some maintainence

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 const int BallCount = 10;
-const int CatagoryCount = 10;
+const int CategoryCount = 10;
 
 float numTOpercentage(int , int);
 float catch_calculator(float, int, int);
+float modifiers(float, float );
+float weather_modifier(int, int);
 
 using std::cout;
 using std::cin;
@@ -12,31 +14,57 @@ using std::endl;
 
 int main() {
 
-    float PokemonHP;
-    int BallType;
-    int PokemonCatagory;
+    float PokemonHP;             // Percentage
+    int BallType;                // 0-9
+    int PokemonCategory;         // 0-9
+    int Weather;                 // (-1,0-9)
+    int Type;                    // int
 
     cout << "Enter Pokemon HP in percentage : ";
     cin >> PokemonHP;
     cout << endl << "Enter BallType : ";
     cin >> BallType;
-    cout << endl << "Enter Pokemon's category : ";
-    cin >> PokemonCatagory;
+    cout << endl << "Enter Pokemon's catch weight category : ";
+    cin >> PokemonCategory;
+    cout << endl << "Enter Weather Condition (-1 for no weather) : ";
+    cin >> Weather;
+    cout << endl << "Enter Pokemon's type : ";
+    cin >> Type;
 
-    float chance = catch_calculator(PokemonHP, BallType, PokemonCatagory);
+    int WeatherModifier = weather_modifier(Weather, Type);
 
-    cout << endl << "You have a " << chance << " % chance of catching the pokemon";
+    float BaseChance = catch_calculator(PokemonHP, BallType, PokemonCategory);
+    float Chance = modifiers(WeatherModifier, BaseChance);
+
+    cout << endl << "You have a " << 100.0f * Chance << " % chance of catching the pokemon";
     
 
 return 0; };
 
-float catch_calculator(float PokemonHP , int BallType, int PokemonCatagory) {
+float catch_calculator(float PokemonHP , int BallType, int PokemonCategory) {
     // PokemonHP is in percentage, BallType is 0-9, PokemonCatagory is 0-9
-    float chance = PokemonHP * numTOpercentage(BallType, BallCount) * numTOpercentage(PokemonCatagory, CatagoryCount) / 1000000;
+    float chance = (100.0f - PokemonHP) * numTOpercentage(BallType, BallCount) * numTOpercentage(PokemonCategory, CategoryCount) / 1000000;
     return chance;
 };
+
+float modifiers(float Weather, float BaseChance){
+    return BaseChance*Weather;
+}
 
 float numTOpercentage(int number, int max) {
     float p = number*100.0f / max;
     return p;
+};
+
+float weather_modifier(int WeatherType, int PokemonType) {
+    // i.e. Evasion
+    // From this logic Weather and pokemon type could be matched for example as (0,sandstorm,ground), (1, rain, water)
+    //Will update to taking a struct or an arr if the need be  - will have to change the input details in main/ fxn call
+    if (WeatherType == -1) {
+        return 1;
+    };
+    if (WeatherType == PokemonType) {
+        return 1.5;
+    };
+    return 1;
 };
