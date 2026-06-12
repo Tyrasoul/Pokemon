0744e8c14621e708920c44fa5800bf93155e6048
Tyrasoul <bs3734ssst@gmail.com>
2026-06-12 10:17:24 +0530

    feat: implemented pokeball mod with Time
    Added normal, great, ultra, dusk, net and master ball.
    +Maintenance : Removed Semicolons after fxns, Improved var names

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-const int BallCount = 10;
+const int PokeBallCount = 10;
 const int CategoryCount = 10;
 
 float numTOpercentage(int , int);
 float catch_calculator(float, int, int);
-float modifiers(float, float );
+float modifiers(float, float, float);
 float weather_modifier(int, int);
+float pokeball_modifiers(int, int, int);
 
 using std::cout;
 using std::cin;
@@ -15,10 +16,11 @@ using std::endl;
 int main() {
 
     float PokemonHP;             // Percentage
-    int BallType;                // 0-9
-    int PokemonCategory;         // 0-9
-    int Weather;                 // (-1,0-9)
-    int Type;                    // int
+    int BallType;                // 1-9
+    int PokemonCategory;         // 1-9
+    int WeatherType;                 // (-1,0-9)
+    int PokemonType;                    // int
+    int Time;
 
     cout << "Enter Pokemon HP in percentage : ";
     cin >> PokemonHP;
@@ -27,29 +29,58 @@ int main() {
     cout << endl << "Enter Pokemon's catch weight category : ";
     cin >> PokemonCategory;
     cout << endl << "Enter Weather Condition (-1 for no weather) : ";
-    cin >> Weather;
+    cin >> WeatherType;
     cout << endl << "Enter Pokemon's type : ";
-    cin >> Type;
+    cin >> PokemonCategory;
+    cout << endl << "Enter Time : ";
+    cin >> Time;
 
-    float WeatherModifier = weather_modifier(Weather, Type);
+    float WeatherModifier = weather_modifier(WeatherType, PokemonType);
+    float PokeballModifier = pokeball_modifiers(BallType, Time, PokemonType);
 
     float BaseChance = catch_calculator(PokemonHP, BallType, PokemonCategory);
-    float Chance = modifiers(WeatherModifier, BaseChance);
+    float Chance = modifiers(WeatherModifier, PokeballModifier, BaseChance);
+
+    if (PokeballModifier == -1){
+        Chance = 1;
+    };
 
     cout << endl << "You have a " << 100.0f * Chance << " % chance of catching the pokemon";
     
 
-return 0; };
+return 0; }
 
 float catch_calculator(float PokemonHP , int BallType, int PokemonCategory) {
 
-    float chance = (100.0f - PokemonHP) * numTOpercentage(BallType, BallCount) * numTOpercentage(PokemonCategory, CategoryCount) / 1000000;
+    float chance = (100.0f - PokemonHP)* numTOpercentage(PokemonCategory, CategoryCount) / 10000;
     return chance;
-};
+}
 
-float modifiers(float WeatherModifier, float BaseChance){
+float modifiers(float WeatherModifier, float PokeballModifier, float BaseChance){
 
-    return BaseChance*WeatherModifier;
+    return BaseChance*WeatherModifier*PokeballModifier;
+}
+
+float pokeball_modifiers(int balltype, int time, int PokemonType){
+    switch(balltype){
+        case 0:
+            return 1;
+        case 1:
+            return 1.5;
+        case 2:
+            return 2;
+        case 9:
+            return -1;
+    };
+
+    if (balltype == 4 and time == 1) {     //night and dusk ball
+        return 3;
+    };
+
+    if (balltype == 4 and (PokemonType == 3 or PokemonType == 4)){    //net ball and water/bug type
+        return 3;
+    };
+    return 1;
 }
 
 float weather_modifier(int WeatherType, int PokemonType) {
@@ -64,9 +95,9 @@ float weather_modifier(int WeatherType, int PokemonType) {
         return 1.5;
     };
     return 1;
-};
+}
 
 float numTOpercentage(int number, int max) {
     float p = number*100.0f / max;
     return p;
-};
+}
